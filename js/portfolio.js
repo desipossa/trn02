a9eb734897e2fa215a18f071a070a2404f808157
desipossa <100889937+desipossa@users.noreply.github.com>
2022-06-27 21:55:02 +0900

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,21 @@
+$(function () {
+    $('#portfolio').fullpage({
+        anchors: ['p01', 'p02', 'p03', 'p04', 'p05'],
+        afterLoad: function (anchorLink, index) {
+            const delayClass = () => {
+                $(this).addClass('on').siblings().removeClass('on');
+            }
+            setTimeout(delayClass)
+
+            $('.nb li').eq(index - 1).addClass('on').siblings().removeClass('on');
+        },
+    });
+
+    $('.cover_open').on('click', function () {
+        $('#cover').toggleClass('on')
+    });
+
+    $('#cover a').on('click', function () {
+        $('#cover').removeClass('on')
+    })
+})
