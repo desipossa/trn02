277d0b7e79c452d4de9b529a00546755d036a877
lee <sipossa@naver.com>
2022-06-28 12:10:29 +0900

    20220628

--- js/main.js
+++ js/main.js
@@ -1,161 +1,125 @@
-// var bannerClose = function () {
-//     document.querySelector('.top_banner').style.display = 'none';
-// }
-
-// document.querySelector('.top_banner i').addEventListener('click', bannerClose);
 $(function () {
-    $('.top_banner i').on('click', function () {
-        $('.top_banner').slideUp();
-    });
+    
+    //20220620 shin topBanner
+    $('.topBanner i').on('click', function(){
+        $('.topBanner').slideUp();
+    })
 
-    $('.main_slider').on('init afterChange', function (e, s, c) {
-        //console.log(e,s,c);//0,1,2
-        //$('.main_slider figure').eq(c+1).addClass('on').siblings().removeClass('on');
-        let current = $('.main_slider .slick-current');
+    $('.mainSlider').on('init afterChange', function (e, s, c) {
+        var current = $('.slick-current');
         current.addClass('on').siblings().removeClass('on');
-        $('.slideNum').text((c ? c + 1 : 1) + "/" + s.slideCount)
+        $('.mainArrow i').removeClass('on')
+        if (c === 1) {
+            $('.mainArrow i').addClass('on')
+        }
+        //if (c) {
+        //    c = c + 1;
+        //} else {
+        //    c = 1;                
+        //}
+        $('.slideNum').text((c ? (c + 1) : 1) + " / " + s.slideCount);
+        //console.log(s)치고 콘솔에서 slideCount 확인      
+       
     });
 
-    $('.main_slider').slick({
-        arrows: false,
-        autoplay: true,
+    $('.mainSlider').slick({
+        arrows:false,
+        //autoplay: true,
         dots: true,
         pauseOnHover: false,
-        pauseOnFocus: false,
+        pauseOnFocus:false,        
     });
 
-    $('.slideArrows i:nth-child(1)').on('click', function () {
-        $('.main_slider').slick('slickPrev')
+    $('.mainArrow i:nth-child(1)').on('click', function () {
+        $('.mainSlider').slick('slickPrev')
+    });
+    $('.mainArrow i:nth-child(2)').on('click', function () {
+        $('.mainSlider').slick('slickNext')
     });
 
-    $('.slideArrows i:nth-child(2)').on('click', function () {
-        $('.main_slider').slick('slickNext')
-    });
 
-    $('.movie_arrows i:nth-child(1)').on('click', function () {
-        $('.movie video').trigger('play');
-    });
-
-    $('.movie_arrows i:nth-child(2)').on('click', function () {
-        $('.movie video').trigger('pause');
-    });
-
-    $('#bgndVideo').YTPlayer({
-        videoURL: 'https://youtu.be/Pmt3tms9HdI',
+    $('#utb').YTPlayer({
+        videoURL: 'https://youtu.be/dS9x5Ws67wI',
         containment: '.utb',
         autoPlay: true,
         mute: true,
         startAt: 0,
         opacity: 1,
         showControls: false,
-        playOnlyIfVisible: true,
+        playOnlyIfVisible:true,
+
     });
 
-    $('.utb i:nth-child(1)').on('click', function () {
-        $('#bgndVideo').YTPPlay();
-    });
-
-    $('.utb i:nth-child(2)').on('click', function () {
-        $('#bgndVideo').YTPPause();
-    });
-
-    $('.utb i:nth-child(3)').on('click', function () {
-        $('#bgndVideo').YTPFullscreen();
-    });
-
-    $('.product_slider').slick({
-        arrows: false,
-        centerMode: true,
-        slidesToShow: 5,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                }
-
-            }
-        ]
-    });
-
-    $('.product_arrows i:nth-child(1)').on('click', function () {
-        $('.product_slider').slick('slickPrev');
-    });
-
-    $('.product_arrows i:nth-child(2)').on('click', function () {
-        $('.product_slider').slick('slickNext');
-    });
-
-    $('.hd_left_slider').slick({
-        arrows: false,
-        fade: true,
-        asNavFor: '.hd_right_slider',
-    });
-    $('.hd_right_slider').slick({
-        arrows: false,
-        slidesToShow: 4,
-        asNavFor: '.hd_left_slider',
-    });
-
-    $('.hd_section .hd_arrows i:nth-child(1)').on('click', function () {
-        $('.hd_right_slider').slick('slickPrev')
-    });
-    $('.hd_section .hd_arrows i:nth-child(2)').on('click', function () {
-        $('.hd_right_slider').slick('slickNext')
-    });
-
-    $('.tab_menu li a').on('click', function (e) {
+    $('.utb .zoom').on('click', function (e) {
         e.preventDefault();
-        var idx = $(this).parent().index();
-        $(this).parent().addClass('on').siblings().removeClass('on');
-        $('.tab_content>div').eq(idx).addClass('on').siblings().removeClass('on')
+        $('#utb').YTPFullscreen();
+    })
+
+    $('.productSlider').slick({
+        arrows:false,
+        //autoplay: true,
+        dots: true,
+        slidesToShow: 5,
+        centerMode:true,     
     });
 
-    $('#fl').on('change', function () {
-        var lik = $(this).val();
-        lik && window.open(lik);
-    });
-
-    $('.to_top').on('click', function () {
-        $('html, body').animate({ scrollTop: 0 }, 1000)
-    });
-
-
-    $(window).on('scroll', function () {
-        var sct = $(window).scrollTop();
-        sct > 800
-            ? $('.to_top').fadeIn()
-            : $('.to_top').fadeOut(1000);
-    });
-
-    $('.mopen').on('click', function () {
-        $(this).toggleClass('on');
-        $('nav').toggleClass('on');
-    });
-
-
-    $('.header nav>ul>li>a').on('click', function (event) {
-        var idx = $(this).parent().index();
-        if ($('nav').hasClass('on') && idx < 3) {
-            event.preventDefault();
-            $(this).next().stop().slideToggle();
-            $(this).parent().siblings().find('ul').slideUp();
-        }
-
-    });
-
-    $(window).on('resize', function () {
-        $('nav').removeClass('on');
-        $('.header ul ul').removeAttr('style')
+    $('.product .productArrows i:nth-child(1)').on('click', function () {
+        $('.productSlider').slick('slickPrev')
+    })
+    $('.product .productArrows i:nth-child(2)').on('click', function () {
+        $('.productSlider').slick('slickNext')
     })
 
 
+    $('.tab_menu li').on('click', function (event) {
+        event.preventDefault();
+        //console.log($(this), $(this).index(), event.target, event.currentTarget);
+        $('.tab_menu li').removeClass('on')
+        $(this).addClass('on');
+        var idx = $(this).index(); // 0, 1, 2
+        $('.tab_content>div').removeClass('on');
+        $('.tab_content>div').eq(idx).addClass('on');
+        //$('.tab_content>div').eq(idx).addClass('on').siblings().removeClass('on');
+    });
 
 
+    $('.tab_content .notice li').on('click', function (event) {
+        event.preventDefault();
+        var idx = $(this).index(); // 0, 1, 2
+        $('.right_content>li').removeClass('on');
+        $('.right_content>li').eq(idx).addClass('on');
+        console.log(ux)
+    });
+
+    $('.toTop').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 200)
+    });
 
 
+    //$(window).on('scroll', function () {
+        //var sct = $(window).scrollTop();
+        //console.log(sct);
+    //}); 이까지하면 스크롤 숫자 console에 찍힘.
 
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        if (sct > 500) {
+            $('.toTop').fadeIn(300);
+        } else {
+            $('.toTop').fadeOut(1000);
+        }
+    });
+
+
+    $('#fl').on('change', function () {
+       // console.log('똑 바로 했나???')
+        console.log($(this), $(this).val())
+        var lnk = $(this).val();
+        //lnk && window.open(lnk);  /* 리액트에서 쓸거다 */          
+        if (lnk) {
+            window.open(lnk);            
+        }     
+        
+    })
 
 })
-
